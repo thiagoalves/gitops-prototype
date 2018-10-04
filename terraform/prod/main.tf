c40a263bf75efd5c4ca15e01c64c75a6299756f6
Thiago Alves Silva <thiago.alves@aurea.com>
2018-10-04 11:46:06 -0300

    First version of terraform prod/stating

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -9,5 +9,5 @@ module "docker_linux" {
   public_key             = "${var.public_key}"
   availability_zone      = "${var.availability_zone}"
 
-  user_data = "${base64encode("touch /tmp/test")}"
+  user_data = "${base64encode("touch /tmp/test-prod")}"
 }
--- terraform/staging/vars.tf
+++ terraform/staging/vars.tf
@@ -13,12 +13,6 @@ variable "public_key" {
   default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDC94OedAUtf5sh+zbX6icJrmR0tGCSI9oJCU4FdMMkXaFPemXdxpyB6qkJnx1sP5inGWBnmy2KdiqtGSQoHN0VW7TvYJzISv9xeMmhDvbWUSF+811Fhdvd7ZzFHClB2vqPjVwyIaTbBCIgus40XPcMnM7TwwSlTsKxKNzottZB+riIsqPGj7FGMcb17TlbWwfyMGyIjSb4f+wwBNF01ydedSJEo11CsZrQ/1B8BNplZrQG5LI7RmsXo6ayXT9Wmia/D0pd3DJT+f5SzsxtLaNC0H046r01IjNUIiockDFULnXLccs9zh4ICP0SnDzxQoTM0nOlolade1Dc9uDMlus1 banjo+thiago.alves@banjo"
 }
 
-variable "vpc_security_group_ids" {
-  type        = "list"
-  description = ""
-  default     = ["sg-606b5507"]
-}
-
 variable "user_data" {
   description = ""
   default     = ""
